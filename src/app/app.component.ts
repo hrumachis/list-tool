7b710863f25b4c44050cb81ecde78ac3c6a05463
Hrumachis <eu.andrius.mikalauskas@gmail.com>
2018-12-03 17:39:02 +0200

    fixed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from './services/auth.service';
 import { Router } from '@angular/router';
 import { DataService } from './services/data.service';
 
@@ -10,5 +11,19 @@ import { DataService } from './services/data.service';
 export class AppComponent {
   busy: boolean = false;
 
-  constructor() { }
+  constructor(private Auth: AuthService, private Database: DataService, private router: Router) { }
+
+  logout() {
+    let self = this;
+
+    if (!this.busy) {
+      this.busy = true;
+
+      setTimeout(function() {
+          self.busy = false;
+          self.Auth.logout();
+          self.router.navigate(['login']);
+      }, 1000);
+    }
+  }
 }

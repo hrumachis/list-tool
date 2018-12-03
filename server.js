a34d94ea2d04703de224699a49e4905877f4bdb0
Hrumachis <eu.andrius.mikalauskas@gmail.com>
2018-12-03 15:47:52 +0200

    fix++

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
+const path = require('path');
 const express = require('express');
 const app = express();
-const path = require('path');
 
-app.use(express.static(__dirname + '/dist'));
-app.listen(process.env.PORT || 8080);
-app.get("/*", function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
-})
+app.use(express.static(__dirname + '/dist/webApp'));
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/webApp/index.html'));
+});
+app.listen(process.env.PORT || 5000);
 
 console.log("Server running");

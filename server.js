b1fbdc69dbfe45713d9360c569fcb9fc980a0dcf
Hrumachis <eu.andrius.mikalauskas@gmail.com>
2018-12-03 16:01:18 +0200

    **

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
-app.use(express.static(__dirname + '/dist/webApp'));
+app.use(express.static(__dirname + '/src'));
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/webApp/index.html'));
+  res.sendFile(path.join(__dirname + '/src/index.html'));
 });
 app.listen(process.env.PORT || 5000);
 
